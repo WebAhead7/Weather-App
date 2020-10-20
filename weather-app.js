6132b3ea9472f4065738400268594bcbbf81f621
Rakad Kh <rakad.khawaled@gmail.com>
2020-10-20 19:46:32 +0300

    finish collecting all the weather data, I costruct 1 object for today and array with next 5 days object each

---
+++ weather-app.js
@@ -0,0 +1,63 @@
+const Key="959c745180588f77a59a13e5043064ef"
+document.querySelector("form").addEventListener("submit", event=>{
+    event.preventDefault();
+    let city=document.getElementById('city').value;
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${Key}&units=metric`)
+    .then(response=>response.json())
+    .then(data => {
+        let date = new Date();
+        let hour = date.getHours();
+        list_today_count=8-Math.floor(hour/3);
+        let nxt5days=[];
+        let this_day={};
+        const today= data=>{
+            curr_temp= data.list[0].main.temp; 
+            curr_weather_type= data.list[0].weather[0].main; 
+            curr_weather_description= data.list[0].weather[0].description; 
+            min_temp=data.list.slice(0, list_today_count).map(item=>item.main.temp_min).reduce((min,temp)=>temp<min? temp: min, data.list.slice(0, list_today_count).map(item=>item.main.temp_min)[0]);
+            max_temp=data.list.slice(0, list_today_count).map(item=>item.main.temp_max).reduce((max,temp)=>temp>max? temp: max, data.list.slice(0, list_today_count).map(item=>item.main.temp_max)[0]);
+            this_day={
+                curr_temp:curr_temp,
+                curr_weather_type:curr_weather_type,
+                curr_weather_description:curr_weather_description,
+                min_temp:min_temp,
+                max_temp:max_temp
+            }
+        }
+        const other_days= data=>{
+            for(let index=0;index<4;index++){
+                    min_temp=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp_min).reduce((min,temp)=>temp<min? temp: min, data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp_min));
+                    max_temp=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp_max).reduce((max,temp)=>temp>max? temp: max, data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp_max));
+                    average_temp=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp).reduce((a, b) => (a + b)) / data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp).length;
+                    closest_to_average=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp).reduce((prev, curr) =>(Math.abs(curr - average_temp) < Math.abs(prev - average_temp) ? curr : prev));
+                    index_of_closest=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1)).map(item=>item.main.temp).indexOf(closest_to_average);
+                    weather_type=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1))[index_of_closest].weather.main;
+                    weather_description=data.list.slice(list_today_count+8*index, list_today_count+8*(index+1))[index_of_closest].weather.description;
+                    nxt5days[index]={
+                        min_temp:min_temp,
+                        max_temp:max_temp,
+                        weather_type:weather_type,
+                        weather_description:weather_description
+                    }
+                }
+                min_temp=data.list.slice(list_today_count+32).map(item=>item.main.temp_min).reduce((min,temp)=>temp<min? temp: min, data.list.slice(list_today_count+32).map(item=>item.main.temp_min));
+                max_temp=data.list.slice(list_today_count+32).map(item=>item.main.temp_min).reduce((max,temp)=>temp>max? temp: max, data.list.slice(list_today_count+32).map(item=>item.main.temp_min));
+                average_temp=data.list.slice(list_today_count+32).map(item=>item.main.temp).reduce((a, b) => (a + b)) / data.list.slice(list_today_count+   32).map(item=>item.main.temp).length;
+                closest_to_average=data.list.slice(list_today_count+32).map(item=>item.main.temp).reduce((prev, curr) =>(Math.abs(curr - average_temp) < Math.abs(prev - average_temp) ? curr : prev));
+                index_of_closest=data.list.slice(list_today_count+32).map(item=>item.main.temp).indexOf(closest_to_average);
+                weather_type=data.list.slice(list_today_count+32)[index_of_closest].weather.main;
+                weather_description=data.list.slice(list_today_count+32)[index_of_closest].weather.description;
+                nxt5days[4]={
+                    min_temp:min_temp,
+                    max_temp:max_temp,
+                    weather_type:weather_type,
+                    weather_description:weather_description
+                }
+            }
+        today(data);
+        other_days(data);
+        console.log(this_day);
+        console.log(nxt5days);
+    })
+    .catch(error => console.log(error));
+});
